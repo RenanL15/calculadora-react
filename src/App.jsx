1f16b23553e21e64a62e7727d32d3459b646f2ca
Renan <renanalmeida201612@gmail.com>
2023-11-24 01:51:44 -0300

    feat: buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,38 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import Buttons from "./components/Buttons";
 
 export default function App() {
+  const [numbers, setNumbers] = useState([]);
+  const [operator, setOperator] = useState("");
+
+  // useEffect(() => {
+  //   setNumbers(numbers.slice(0, 10));
+  // }, [numbers]);
+
   return (
-    <main className="min-h-screen flex justify-center items-center bg-slate-500">
-      <div className="bg-slate-200 p-5">
-        <div className="">
+    <main className="min-h-screen flex justify-center items-center bg-gray-400">
+      <div className="bg-zinc-700 p-8 max-w-[30rem] mx-6 rounded-lg shadow-2xl">
+        <div className="flex flex-col bg-white mb-8 rounded-lg px-4 py-2">
+          <span
+            className={`ml-auto ${operator != "" ? "visible" : "invisible"}`}>
+            {numbers.length === 0 ? 0 : numbers.join("")}{" "}
+            {operator ? operator : 0}
+          </span>
           <input
-            type="number"
-            className="caret-transparent text-end text-2xl font-bold"
+            type="text"
+            onBlur={(e) => e.target.focus()}
+            value={numbers.length === 0 ? 0 : numbers.join("").slice(0, 12)}
+            autoFocus
+            maxLength={4}
+            className="pointer-events-none caret-transparent bg-transparent text-end text-5xl font-bold w-full"
           />
         </div>
+        <Buttons
+          numbers={numbers}
+          setNumbers={setNumbers}
+          operator={operator}
+          setOperator={setOperator}
+        />
       </div>
     </main>
   );
---
+++ src/components/Buttons.jsx
@@ -0,0 +1,142 @@
+import {
+  FaBackspace,
+  FaDivide,
+  FaEquals,
+  FaMinus,
+  FaPercentage,
+  FaPlus,
+  FaTimes,
+} from "react-icons/fa";
+import { PiPlusMinusBold } from "react-icons/pi";
+
+export default function Buttons({
+  numbers,
+  setNumbers,
+  operator,
+  setOperator,
+}) {
+  return (
+    <div className="grid grid-cols-4 gap-4 text-center text-2xl">
+      <button
+        onClick={() => setNumbers([...numbers, 7])}
+        className="p-5 bg-yellow-500 hover:bg-yellow-400 rounded-lg">
+        <FaPercentage className="m-auto" />
+      </button>
+      <button
+        onClick={() => {
+          setOperator("");
+          setNumbers([]);
+        }}
+        className="p-5 bg-yellow-500 font-bold hover:bg-yellow-400 rounded-lg">
+        C
+      </button>
+      <button
+        onClick={() => {
+          const popArr = [...numbers];
+          popArr.pop();
+          setNumbers(popArr);
+        }}
+        className="p-5 bg-yellow-500 hover:bg-yellow-400 rounded-lg">
+        <FaBackspace className="m-auto" />
+      </button>
+      <button
+        onClick={() => setOperator("÷")}
+        className="p-5 bg-slate-400 hover:bg-slate-500 text-white rounded-lg">
+        <FaDivide className="m-auto" />
+      </button>
+      <button
+        onClick={() => setNumbers([...numbers, 7])}
+        className="p-5 bg-zinc-200 hover:bg-zinc-300 rounded-lg">
+        7
+      </button>
+      <button
+        onClick={() => setNumbers([...numbers, 8])}
+        className="p-5 bg-zinc-200 hover:bg-zinc-300 rounded-lg">
+        8
+      </button>
+      <button
+        onClick={() => setNumbers([...numbers, 9])}
+        className="p-5 bg-zinc-200 hover:bg-zinc-300 rounded-lg">
+        9
+      </button>
+      <button
+        onClick={() => setOperator("x")}
+        className="p-5 bg-slate-400 hover:bg-slate-500 text-white rounded-lg">
+        <FaTimes className="m-auto" />
+      </button>
+      <button
+        onClick={() => setNumbers([...numbers, 4])}
+        className="p-5 bg-zinc-200 hover:bg-zinc-300 rounded-lg">
+        4
+      </button>
+      <button
+        onClick={() => setNumbers([...numbers, 5])}
+        className="p-5 bg-zinc-200 hover:bg-zinc-300 rounded-lg">
+        5
+      </button>
+      <button
+        onClick={() => setNumbers([...numbers, 6])}
+        className="p-5 bg-zinc-200 hover:bg-zinc-300 rounded-lg">
+        6
+      </button>
+      <button
+        onClick={() => setOperator("+")}
+        className="p-5 bg-slate-400 hover:bg-slate-500 text-white rounded-lg">
+        <FaPlus className="m-auto" />
+      </button>
+      <button
+        onClick={() => setNumbers([...numbers, 1])}
+        className="p-5 bg-zinc-200 hover:bg-zinc-300 rounded-lg">
+        1
+      </button>
+      <button
+        onClick={() => setNumbers([...numbers, 2])}
+        className="p-5 bg-zinc-200 hover:bg-zinc-300 rounded-lg">
+        2
+      </button>
+      <button
+        onClick={() => setNumbers([...numbers, 3])}
+        className="p-5 bg-zinc-200 hover:bg-zinc-300 rounded-lg">
+        3
+      </button>
+      <button
+        onClick={() => setOperator("-")}
+        className="p-5 bg-slate-400 hover:bg-slate-500 text-white rounded-lg">
+        <FaMinus className="m-auto" />
+      </button>
+      <button
+        onClick={() => {
+          const shArr = [...numbers];
+          shArr.shift();
+          numbers.includes("-")
+            ? setNumbers(shArr)
+            : numbers.length > 0 && setNumbers(["-", ...numbers]);
+        }}
+        className="p-5 bg-zinc-200 hover:bg-zinc-300 rounded-lg">
+        <PiPlusMinusBold className="m-auto" />
+      </button>
+      <button
+        onClick={() => {
+          numbers.length === 0 ? setNumbers([]) : setNumbers([...numbers, 0]);
+        }}
+        className="p-5 bg-zinc-200 hover:bg-zinc-300 rounded-lg">
+        0
+      </button>
+      <button
+        onClick={() => {
+          !numbers.includes(".") &&
+            (numbers.length === 0
+              ? setNumbers([0, "."])
+              : setNumbers([...numbers, "."]));
+        }}
+        className="p-5 bg-zinc-200 hover:bg-zinc-300 rounded-lg">
+        .
+      </button>
+      <button
+        onClick={() => setNumbers([...numbers, 0])}
+        className="p-5 bg-red-700 text-white hover:bg-red-800 rounded-lg">
+        <FaEquals className="m-auto" />
+      </button>
+    </div>
+  );
+}
